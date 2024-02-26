902dc23dfc47c5ce1a35dd7c9ba690bb7449ac20
Jewoo Ham <jeus0630@gmail.com>
2024-02-26 15:53:45 +0900

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction cicd Hi !! ~ g
+          Learn GithubAction cicd Hi !! ~
         </a>
       </header>
     </div>
